785b3d246a978256d917204e4057313087935407
Philip Han <46556419+Philipnah@users.noreply.github.com>
2023-02-01 19:13:13 +0100

    Original script. Future changes may come.

---
+++ morsBil.py
@@ -0,0 +1,14 @@
+# mors bils specs
+KMperL = 20
+KMperkW = 5
+
+print("Programmet favoriserer benzin lidt!\n")
+
+gasPrice = float(input("Benzin pris (kr/L): "))
+elPrice = float(input("El pris (kr/kWh): "))
+
+pricePerKMgas = gasPrice/KMperL
+pricePerKMElectric = elPrice/KMperkW
+
+print("\n-----------------------------\nBenzin: " + str(pricePerKMgas) + " kr/km")
+print("El: " + str(pricePerKMElectric) + " kr/km\n-----------------------------\n")
